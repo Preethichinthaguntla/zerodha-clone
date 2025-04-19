c3caf2a3cf1e5c6ee1af071b363d434a53e7ae9e
Preethichinthaguntla <chinthaguntlapreethi@gmail.com>
2025-04-19 20:50:01 +0530

    Re-added frontend folder as normal folder

---
+++ frontend/src/landing_page/signup/OpenAccount.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function OpenAccount() {
+  return (
+    <div className="container">
+      <h2>Open an Account</h2>
+      <p>Fill out the form to open a new account with us.</p>
+      {/* Add form elements here */}
+    </div>
+  );
+}
+
+export default OpenAccount;
